75977efd1a9fb3a577fb041888bb68f229c434f0
Umar Sulaiman <akoredeumar8@gmail.com>
2025-02-22 21:50:32 +0100

    Fix Security Vulnerability

--- src/SignUp/Leftside.jsx
+++ src/SignUp/Leftside.jsx
@@ -1,51 +1,46 @@
-import { Box, Text, Image, Flex } from "@chakra-ui/react";
-import React from "react";
-import esvg from "../svgs/esvg.svg";
-import cartoon from "../svgs/cartoon.svg";
-import dots from "../svgs/dots.svg";
-import lg from "../svgs/lg.svg";
+import { Box, Text, Image } from '@chakra-ui/react'
+import React from 'react'
+import esvg from '../svgs/esvg.svg'
+import cartoon from '../svgs/cartoon.svg'
+import dots from '../svgs/dots.svg'
 
 const Leftside = () => {
   return (
-    <>
-      <Box
-        w="50%"
-        bg="#103C29"
-        pb="45px"
-        h="-moz-max-content"
-        width={{ lg: "50%", md: "100%" }}
+    <Box w={{ lg: '50%', md: '100%' }} bg="#103C29" pb="45px" h="max-content">
+      {/* HEADING TEXT */}
+      <Text
+        as="p"
+        textAlign="center"
+        pt="99px"
+        color="#ffffff"
+        noOfLines={1}
+        maxW="80%"
+        mx="auto"
       >
-        {/* HEADING TEXT */}
-        <Text as="p" textAlign="center" pt="99px" color="#ffffff" isTruncated>
-          Powering businesses with seamless digital payments
-        </Text>
+        Powering businesses with seamless digital payments
+      </Text>
 
-        {/* ELLIPSE CONTAINER */}
-        <Box
-          textAlign="center"
-          alignItems="center"
-          pt="50px"
-          position="relative"
-          w="100%"
-        >
-          {/* Background SVG */}
-          <Image src={esvg} w="82%" ml="8%" />
+      {/* ELLIPSE CONTAINER */}
+      <Box textAlign="center" pt="50px" position="relative" w="100%">
+        <Image src={esvg} w="82%" ml="8%" />
 
-          {/* CARTOON IMAGE INSIDE */}
-          <Image
-            src={cartoon}
-            w="65%"
-            position="absolute"
-            top="55%"
-            left="50%"
-            transform="translate(-50%, -50%)"
-          />
-        </Box>
-
-        {/* STATIC DOTS FOR SLIDER */}
+        {/* CARTOON IMAGE INSIDE */}
+        <Image
+          src={cartoon}
+          w="65%"
+          position="absolute"
+          top="55%"
+          left="50%"
+          transform="translate(-50%, -50%)"
+        />
       </Box>
-    </>
-  );
-};
 
-export default Leftside;
+      {/* STATIC DOTS FOR SLIDER */}
+      <Box textAlign="center" mt="20px">
+        <Image src={dots} w="50px" mx="auto" />
+      </Box>
+    </Box>
+  )
+}
+
+export default Leftside

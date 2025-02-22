b749b2dea1cea3ccad7635fd1807ed520dd32f60
Terna Nanev <ternathompson2@gmail.com>
2025-02-22 04:33:15 +0100

    Add relative import

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -1,11 +1,12 @@
 import { Box, Image, Text } from "@chakra-ui/react";
+import userImage from "../assets/userImage.svg";
 
 function UserProfile() {
   return (
     <Box display="flex" flexDirection="row" alignItems="center" gap="4">
       <Image
         w="50px"
-        src="src/assets/userImage.svg"
+        src={userImage}
         alt="profile-image"
         cursor="pointer"
         loading="lazy"

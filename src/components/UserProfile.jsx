61ea3c6cd5aafed7f01323aa670ed30e0651bfd8
Terna Nanev <ternathompson2@gmail.com>
2025-02-25 13:50:30 +0100

    Update style for better appearance

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -4,19 +4,25 @@ import userImage from "../assets/userImage.svg";
 function UserProfile() {
   return (
     <Box
+      w="full"
       display="flex"
       flexDirection="row"
       alignItems="center"
       justifyContent={{ base: "end" }}
+      alignSelf="center"
       gap="4"
+      // border="sm"
+      // borderColor="black"
     >
-      <Image
-        w="50px"
-        src={userImage}
-        alt="profile-image"
-        cursor="pointer"
-        loading="lazy"
-      />
+      <Box boxSize="50px">
+        <Image
+          w="50px"
+          src={userImage}
+          alt="profile-image"
+          cursor="pointer"
+          loading="lazy"
+        />
+      </Box>
       <Text
         fontWeight="medium"
         fontSize="16px"

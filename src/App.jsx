53c5ef6abf507d84fcc308a1806989b8366cf729
Abass Ibrahim <profbass4all@gmail.com>
2025-02-26 05:26:52 +0100

    setup chakra and created the heading of the component

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import { Box } from "@chakra-ui/react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Box bg="white" width="80">
+      <Box
+        color="#313431"
+        fontSize="md"
+        fontWeight="medium"
+        textAlign={"center"}
+        pb="4"
+        pt="4"
+      >
+        Transaction volume by Region
+      </Box>
+    </Box>
+  );
 }
 
 export default App

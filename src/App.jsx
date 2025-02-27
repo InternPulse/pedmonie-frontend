fb9aeca069a286a01a58cb49e293ab718549d003
omobolaji <omobolaji119@gmail.com>
2025-02-28 00:23:43 +0100

    added transaction status

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import { Box } from "@chakra-ui/react"
-
+import TotalRevenue from "./components/TotalRevenue"
+import TotalRevenueBarChart from "./components/TotalRevenueBarChart"
+import TransactionStatus from "./components/TransactionStatus"
 const App = () => {
   return (
       <Box>
+        <TransactionStatus />
       </Box>
   )
 }

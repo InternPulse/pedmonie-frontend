ab648c68113d94ff0b6102115823f87cc3f17baa
Terna Nanev <ternathompson2@gmail.com>
2025-03-04 01:32:31 +0100

    Modify App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import MainLayout from "./components/Layout/MainLayout";
 import Loading from "./components/Loading";
 import AuthPage from "./pages/AuthPage";
 import Signup from "./SignUp/SignUp";
-import "./index.css";
+// import "./index.css";
 
 function App() {
   const Home = lazy(() => import("./pages/Home"));

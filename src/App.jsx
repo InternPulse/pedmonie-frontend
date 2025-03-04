f0601bfab9c6bea140c9ca814bcdd9db66520c34
Abass Ibrahim <profbass4all@gmail.com>
2025-03-04 01:15:51 +0100

    I designed a section from the landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import { Box, Text } from '@chakra-ui/react'
+import Section from './components/Section'
 
 const App = () => {
   return (
     <>
+
+      <Section />
       <Box>
         <Text>Hello</Text>
       </Box>

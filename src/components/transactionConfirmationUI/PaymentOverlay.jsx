efebf92888309be51c7e6f35df2c79f63fe3929e
Terna Nanev <ternathompson2@gmail.com>
2025-03-07 19:30:07 +0100

    Modify buttons

--- src/components/transactionConfirmationUI/PaymentOverlay.jsx
+++ src/components/transactionConfirmationUI/PaymentOverlay.jsx
@@ -32,7 +32,7 @@ function PaymentOverlay() {
               color="white"
               px={4}
               py={2}
-              borderRadius="0"
+              disabled={value !== "New Payment Link" ? true : false}
             >
               {value}
               {value === "All Payment Link" && <FaChevronDown />}
